8a6e26f07e6aa6cc9bd139d66bebc7c834435064
Mustaf <ahmedmustaf0@gmail.com>
2021-06-30 14:43:17 +0530

    Using ES6 standards

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,8 +1,8 @@
 export const fetchDemo = (body) => {
   let arr = [];
   body.Recommendations.forEach((element) => {
-    var obj = {};
-    // console.log(element.RestaurantName);
+    let obj = {};
+    console.log(element.RestaurantName);
     obj.name = element.RestaurantName;
 
     obj.childName = [];
@@ -12,10 +12,10 @@ export const fetchDemo = (body) => {
         mymenu.children.forEach((child) => {
           if (child.type === "item") {
             if (child.selected === 1) {
-              var childobj = {};
-              //   console.log("-->", child.name);
+              let childobj = {};
+              console.log("-->", child.name);
               childobj.name = child.name;
-              var childName = recursion(child.children, 1);
+              let childName = recursion(child.children, 1);
               if (childName.length > 0) childobj.childName = childName;
               obj.childName.push(childobj);
             }
@@ -32,17 +32,17 @@ function recursion(children, i) {
   if (!children) return;
   if (children === undefined) return;
   i++;
-  var childobj = [];
+  let childobj = [];
   children.forEach((child) => {
-    // var string = "";
-    // var j = 0;
-    // for (j = 0; j < i; j++) string += "--";
-    // string += ">";
+    let string = "";
+    let j = 0;
+    for (j = 0; j < i; j++) string += "--";
+    string += ">";
     if (child.selected === 1) {
-      var children = {};
-      //   console.log(string, child.name);
+      let children = {};
+      console.log(string, child.name);
       children.name = child.name;
-      var childName = recursion(child.children, i);
+      let childName = recursion(child.children, i);
       if (childName.length > 0) children.childName = childName;
       childobj.push(children);
     }
